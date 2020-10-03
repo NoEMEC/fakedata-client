a7275d808abdc0d64f2a3cd0911c1b3bb9feda9a
Ruben Carrascal <krrskl@MacBook-Pro-de-Ruben.local>
2020-10-02 19:52:08 -0500

    :sparkles: Creating the documentation view.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,22 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import Home from '../views/Home.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/',
+    name: 'Home',
+    component: Home
   },
+  {
+    path: '/documentation',
+    name: 'documentation',
+    component: () => import('@/views/Documentation.vue')
+  }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes
 });
 
 export default router;
---
+++ src/views/Documentation.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="documentation"></div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class Documentation extends Vue {}
+</script>
